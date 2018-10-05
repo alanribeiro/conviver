aa3cd10187c5b173a27dc58e86f9c936df465be9
AlanRibeiro <alanribeirodsantos@gmail.com>
2018-10-05 16:21:03 -0300

    fullWidth e margin

--- src/app/atoms/co-select/co-select.component.ts
+++ src/app/atoms/co-select/co-select.component.ts
@@ -9,8 +9,13 @@ export class CoSelectComponent {
 
   @Input() placeholder:string;
   @Input() options:Array<{value:string, label:string}>;
+  @Input() fullWidth:boolean;
+  @Input() margin:boolean;
   selectedValue:string;
 
-  constructor() {}
+  constructor() {
+    this.fullWidth = false;
+    this.margin = false;
+  }
 
 }
