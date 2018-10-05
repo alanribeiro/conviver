3012b4231534d385db34aa0e07a731bfc2c0a900
KIQ19 <kiqaraujo97@gmail.com>
2018-10-05 16:55:22 -0300

    Revert "Adicionando o atomo co-stepper-dots-dot"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { CoRoundedButtonComponent } from './atoms/co-rounded-button/co-rounded-b
 import { CoMenuItemComponent } from './atoms/co-menu-item/co-menu-item.component';
 import { CoMenuItemsComponent } from './molecules/co-menu-items/co-menu-items.component';
 import { CoAvatarComponent } from './atoms/co-avatar/co-avatar.component';
-import { CoStepperDotsDotComponent } from './atoms/co-stepper-dots-dot/co-stepper-dots-dot.component';
 import { CoMainHeaderComponent } from './organisms/co-main-header/co-main-header.component';
 import { CoMobileMenuButtonComponent } from './atoms/co-mobile-menu-button/co-mobile-menu-button.component';
 import { CoNotificationButtonComponent } from './atoms/co-notification-button/co-notification-button.component';
@@ -40,7 +39,6 @@ registerLocaleData(en);
     CoMenuItemComponent,
     CoMenuItemsComponent,
     CoAvatarComponent,
-    CoStepperDotsDotComponent
     CoMainHeaderComponent,
     CoMobileMenuButtonComponent,
     CoNotificationButtonComponent,
