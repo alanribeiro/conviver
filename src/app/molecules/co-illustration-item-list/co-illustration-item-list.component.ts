de8dfb7e347af9e8204b1edcacad5508a5061784
Mateus <31441471+Mathenr@users.noreply.github.com>
2018-10-11 21:28:03 -0300

    Update co-illustration-item-list.component.ts

--- src/app/molecules/co-illustration-item-list/co-illustration-item-list.component.ts
+++ src/app/molecules/co-illustration-item-list/co-illustration-item-list.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class CoIllustrationItemListComponent {
 
-  @Input() itemsList: Array<Object>
+  @Input() itemsList: Array<{icon:string, name:string}>
 
   constructor() { }
 
